424b6efa94b968af7fb0f9c79672304610e1d388
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-22 09:57:25 -0400

    added c prompts

---
+++ src/CPrompt.py
@@ -0,0 +1,6 @@
+class Prompts:
+    def CPrompt():
+        ExtentionType = input("What type of extention do you want the output to be? Ex. exe \n")
+        ProjectName = input("Whats the name of the project?")
+        Flags = input("What other flags would you like to add? \n -g ")
+        
