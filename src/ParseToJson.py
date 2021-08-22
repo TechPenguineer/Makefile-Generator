7e29cff34eb6447157094944e8f7ba59e28f0419
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-22 10:29:15 -0400

    ,

--- src/ParseToJson.py
+++ src/ParseToJson.py
@@ -1,10 +1,12 @@
 from os import sync
 from init import *
 
+class temps:
+    class c:
+        
 
 class C:
      def prompt():
-        CTEMP = open("temp/c/Makefile", "r")
         Prompts.CPrompt()
         JSON_DATA = json.encoder(CTEMP)
         print(JSON_DATA)
